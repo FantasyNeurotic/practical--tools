4d40fc783a64363472e276af3a4e7b9353eb6a26
FantasyNeurotic <953280643@qq.com>
2017-09-07 15:36:34 +0800

    my dict

---
+++ dict/index.js
@@ -0,0 +1,49 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const chalk = require('chalk');
+const fs = require('fs');
+const Spinner = require('cli-spinner').Spinner;
+const spinner = new Spinner('学好英语吧，再也不见... %s');
+const home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+const configFile = home + "/config.json";
+let color = 'white';
+
+
+spinner.setSpinnerString('|/-\\');
+spinner.start();
+
+
+
+const readFile = (filename, encoding) => {
+
+    try {
+        return fs.readFileSync(filename).toString(encoding);
+    }
+    catch (e) {
+        return null;
+    }
+};
+
+const config = JSON.parse(readFile(configFile,"utf8"));
+console.log(process.argv)
+const word = process.argv.slice(2)[0];
+const URL = `http://dict.youdao.com/w/${word}`
+const options = {
+  'url':URL
+};
+
+if(config){
+  if(config.proxy){
+    options.proxy = config.proxy;
+  }
+  if(config.color){
+    color = config.color;
+  }
+}
+const color_output = chalk.keyword(color);
+request(options,(error, response, body)=>{
+
+  const $ = cheerio.load(body);
+  spinner.stop($);
+  console.log(color_output($('div#phrsListTab > div.trans-container > ul').text()));
+});
